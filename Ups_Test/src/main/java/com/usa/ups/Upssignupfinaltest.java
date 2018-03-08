81c81cd8259b3ca1ccd0dbeca8539234f190cae6
Mohammad Ali <Mohammad Ali@DESKTOP-SVCS4KF>
2018-03-08 10:58:04 -0500

    Ups final

--- Ups_Test/src/main/java/com/usa/ups/Upssignupfinaltest.java
+++ Ups_Test/src/main/java/com/usa/ups/Upssignupfinaltest.java
@@ -20,17 +20,14 @@ public class Upssignupfinaltest {
 	public void user_able_to_open_browser() throws Throwable {
 		
 	   driver= new FirefoxDriver();
-	   
-	   
-	    
+	     
 	}
 
 	@Given("^go to UPS home page$")
 	public void go_to_UPS_home_page() throws Throwable {
 		
 		 driver.navigate().to("https://www.ups.com/us/en/Home.page");
-		   driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-	   
+		 driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 	   
 	}
 
@@ -38,38 +35,34 @@ public class Upssignupfinaltest {
 	public void user_navigate_signup_button_link() throws Throwable {
 		
 		driver.findElement(By.xpath(".//*[@id='ups-navItems']/ul/li[2]/a")).click();
-	   
 	    
 	}
 
 	@When("^User put user name-Sumon$")
 	public void user_put_user_name_Sumon() throws Throwable {
 		 driver.findElement(By.xpath(".//*[@id='ups-full_name_input']")).sendKeys("Sumon");
-	    
-	    
+ 
 	}
 
 	@When("^User put valid user email-sumss(\\d+)@yahoo\\.com$")
 	public void user_put_valid_user_email_sumss_yahoo_com(int arg1) throws Throwable {
 		
-		 driver.findElement(By.xpath(".//*[@id='ups-email_input']")).sendKeys("sumss04@yahoo.com");
-	    
+		 driver.findElement(By.xpath(".//*[@id='ups-email_input']")).sendKeys("sumss04@yahoo.com"); 
 	    
 	}
 
 	@When("^User put user id-Sumss(\\d+)cdm$")
 	public void user_put_user_id_Sumss_cdm(int arg1) throws Throwable {
 		
-		 driver.findElement(By.xpath(".//*[@id='ups-user_id_input']")).sendKeys("Sumss1238cdm");
+		 driver.findElement(By.xpath(".//*[@id='ups-user_id_input']")).sendKeys("Sumss1238cdn");
 	    
 	}
 
 	@When("^User put valid password-M(\\d+)a\\$\\$$")
 	public void user_put_valid_password_M_a$$(int arg1) throws Throwable {
 		
-		 driver.findElement(By.xpath(".//*[@id='ups-user_password_input']")).sendKeys("M131187a$$");
-	    
-	    
+		 driver.findElement(By.xpath(".//*[@id='ups-user_password_input']")).sendKeys("M131187a$$$");
+	      
 	}
 
 	@When("^User able to Nevigate to check box button link$")
@@ -77,13 +70,11 @@ public class Upssignupfinaltest {
 		
 		 driver.findElement(By.xpath(".//*[@id='ups-checkbox_group']/div/label")).sendKeys(Keys.SPACE);
 	    
-	   
 	}
 
 	@When("^User able to click continue button$")
 	public void user_able_to_click_continue_button() throws Throwable {
-		 driver.findElement(By.xpath(".//*[@id='SignupDiv']/div[1]/div/div/div[2]/div[2]/form/div[4]/div/button")).sendKeys(Keys.SPACE);
-	    
+		 driver.findElement(By.xpath(".//*[@id='SignupDiv']/div[1]/div/div/div[2]/div[2]/form/div[4]/div/button")).sendKeys(Keys.SPACE);  
 	    
 	}
 
@@ -93,7 +84,6 @@ public class Upssignupfinaltest {
 		  driver.findElement(By.xpath(".//*[@id='SignupConfirmationDiv']/div[1]/div/div/div[2]/div[4]/div/button")).sendKeys(Keys.SPACE);
 	        
 	        System.out.println("Test has passed");
-	    
 	    
 	}
 	
